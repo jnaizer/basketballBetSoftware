5795c5c6ef1d6dd7d309e61ff7d87c4ce7921bdf
jnaizer <85801903+jnaizer@users.noreply.github.com>
2021-06-14 21:58:40 -0400

    Add files via upload
    Changed up the code and made it so it deletes the unnecessary empty columns and makes a neat hash (#) separated file to import into RStudio

--- DataScraping.py
+++ DataScraping.py
@@ -1,16 +1,17 @@
+from urllib.request import urlopen
+import random
+import requests
+import time
 from bs4 import BeautifulSoup
 import pandas as pd
 pd.set_option('display.max_colwidth', 500)
-import time
-import requests
-import random
-from urllib.request import urlopen
 
 # NBA season we will be analyzing
 year = 2019
 team = "UTA"
 # URL page we will scraping (see image above)
-url = "https://www.basketball-reference.com/teams/{}/2021_games.html".format(team)
+url = "https://www.basketball-reference.com/teams/{}/{}_games.html".format(
+    team, year)
 # this is the HTML from the given URL
 html = urlopen(url)
 soup = BeautifulSoup(html)
@@ -26,7 +27,37 @@ print(headers)
 # avoid the first header row
 rows = soup.findAll('tr')[1:]
 player_stats = [[td.getText() for td in rows[i].findAll('td')]
-            for i in range(len(rows))]
-stats = pd.DataFrame(player_stats, columns = headers)
+                for i in range(len(rows))]
+stats = pd.DataFrame(player_stats, columns=headers)
 stats.head(10)
 print(stats)
+
+stats.to_csv(r"Python Projects\BasketballBetting\file1", sep="#", index=False)
+# file1 = open(r"Python Projects\BasketballBetting\file1", "r")
+# file2 = open(r"Python Projects\BasketballBetting\file2", "w")
+
+first = True
+
+with open(r"Python Projects\BasketballBetting\file1", "r") as f:
+    lines = f.readlines()
+with open(r"Python Projects\BasketballBetting\file1", "w") as f:
+    for line in lines:
+        if (line.__contains__("######")):
+            continue
+        list = line.split("#")
+        if (first):
+            list[4] = "Location"
+            list[6] = "Outcome"
+            first = False
+        else:
+            if (list[4] == "@"):
+                list[4] = "Away"
+            else:
+                list[4] = "Home"
+        del list[2]
+        del list[2]
+        del list[5]
+        del list[10]
+        result = "#".join(list)
+        f.write(result + "\n")
+
